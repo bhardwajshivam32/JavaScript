46654b5ab84eb482f6865f9c37c28f659f7be790
bhardwajshivam32 <bhardwajshivam32@gmail.com>
2023-10-26 23:48:29 +0530

    finished while loop

--- while loop eg.js
+++ while loop eg.js
@@ -1,12 +1,15 @@
 // while loop example
 
 let num = 10;
-let total = 0; // 1+2+3
-let i = 0;
+// let total = 0; // 1+2+3
+// let i = 0;
 
-while(i<=100){
-    total = total + i;
-    i++; 
-}
+// while(i<=100){
+//     total = total + i;
+//     i++; 
+// }
 
+// console.log(total);
+
+let total = (num*(num+1))/2;
 console.log(total);
