bafc5f48d2dda048d9103a823c4ca685d92ed6f5
bhardwajshivam32 <bhardwajshivam32@gmail.com>
2023-10-29 21:13:39 +0530

    difference between dot and bracket notation

---
+++ dot and bracket notation.js
@@ -0,0 +1,11 @@
+// difference between dot and bracket notation
+const key = "email";
+const person = {
+    name: "Shivam",
+    age: "22",
+    "person hobbies": ["guitar", "sleeping", "listening music"]
+}
+
+console.log(person["person hobbies"]);
+person[key] = "bhardwajshivam32@gmail.com";
+console.log(person); 
